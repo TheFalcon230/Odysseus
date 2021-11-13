83d80f4f17d6dd5c9002eae39c8ce38359c2e13a
TheFalcon230 <yanis.gabet@gmail.com>
2021-11-13 20:44:57 +0100

    Tests

--- Odysseus/src/Odysseus/Application.cpp
+++ Odysseus/src/Odysseus/Application.cpp
@@ -1,16 +1,18 @@
 #include "odcpch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
-#include "Log.h"
+#include "Odysseus/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Odysseus
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		
 	}
 
 	Application::~Application()
@@ -22,8 +24,24 @@ namespace Odysseus
 		
 		while (m_Running) 
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->Update();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		ODC_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Odysseus/src/Odysseus/Application.h
+++ Odysseus/src/Odysseus/Application.h
@@ -1,6 +1,9 @@
 #pragma once
+
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Odysseus
@@ -12,7 +15,11 @@ namespace Odysseus
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
